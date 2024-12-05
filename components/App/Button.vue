716a20fd696a349b3aa317715a603b4cd056acb5
busefilizler <b.filizler@digitastic.app>
2024-12-05 16:56:24 +0300

    refactor: clean up Button.vue component; improve code formatting and consistency

--- components/App/Button.vue
+++ components/App/Button.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { RouteRecordRaw } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router'
 
 const STYLES = {
   primary: {
@@ -11,31 +11,34 @@ const STYLES = {
     all: 'text-white uppercase font-semibold sm:font-bold group px-4 py-2 relative',
     main: 'relative z-50',
     before: 'absolute bg-gradient-to-r from-green-600 to-green-900 top-0 left-0 w-full h-full z-10',
-    after: 'group-hover:hidden absolute top-0.5 left-0.5 bottom-0 right-0 w-[calc(100%-0.25rem)] h-[calc(100%-0.25rem)] z-10'
-  }
+    after: 'group-hover:hidden absolute top-0.5 left-0.5 bottom-0 right-0 w-[calc(100%-0.25rem)] h-[calc(100%-0.25rem)] z-10',
+  },
 } as const
 
 type Style = keyof typeof STYLES
 
 const props = withDefaults(defineProps<{
-  look?: Style,
-  secondaryAfterBg?: string,
+  look?: Style
+  secondaryAfterBg?: string
   to?: string | RouteRecordRaw
 }>(), {
   look: 'primary',
-  secondaryAfterBg: 'bg-black'
+  secondaryAfterBg: 'bg-black',
 })
 
 const selectedLook = computed(() => STYLES[props.look])
 const AppLink = resolveComponent('AppLink')
 const componentToRender = computed(() => props.to ? AppLink : 'button')
-
 </script>
 
 <template>
   <component
-:is="componentToRender" :type="to ? undefined : 'button'" :class="selectedLook.all" :to="to"
-    class="focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 inline-block">
+    :is="componentToRender"
+    :type="to ? undefined : 'button'"
+    :class="selectedLook.all"
+    :to="to"
+    class="focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 inline-block"
+  >
     <template v-if="selectedLook.needsPseudo">
       <span :class="selectedLook.before" />
       <span :class="'main' in selectedLook && selectedLook.main">
