39934aad91a2a4b03957f6773534a19bf2e654ba
busefilizler <b.filizler@digitastic.app>
2024-12-05 14:32:46 +0300

    refactor: adjust margins in About and Contact components; set background color to black

--- pages/contact.vue
+++ pages/contact.vue
@@ -14,7 +14,7 @@
         questions, or feedback or just want to say hi, feel free to reach out to
         me on any of the platforms linked in the footer or via email.
       </AppParagraph>
-      <AppButton class="mr-4 mt-12">
+      <AppButton class="mr-4 mt-20">
         <a href="mailto:filizlerbuse@gmail.com">Send email</a>
       </AppButton>
       <AppButton
@@ -27,9 +27,18 @@
         >Download Resume</a>
       </AppButton>
 
-      <AppParagraph class="mt-12 text-lg max-w-prose">
+      <AppParagraph class="mt-20 text-lg max-w-prose">
         I usually respond within 24 to 48 hours.
       </AppParagraph>
     </AppSection>
+    <AppSection />
+    <AppSection />
   </div>
 </template>
+
+<style>
+.__nuxt, html {
+  background-color: black !important;
+  height: 100vh !important;
+}
+</style>
